d4206bf1641c40592fcbaec2ac38401b1411fcca
Daniel Chin <danielsimonchin@hotmail.com>
2024-04-05 14:40:27 -0400

    db tweak

--- server/db.js
+++ server/db.js
@@ -27,6 +27,8 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 db.getConnection((err) => {
