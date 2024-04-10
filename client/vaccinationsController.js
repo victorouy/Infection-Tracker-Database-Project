c247bfc4de4a7afed6d68c03e6f133b1811382b7
Daniel Chin <danielsimonchin@hotmail.com>
2024-04-10 15:11:12 -0400

    fix for vacinnation update

--- client/vaccinationsController.js
+++ client/vaccinationsController.js
@@ -1,71 +1,73 @@
 function getAllVaccinations() {
-    fetch(`${BASE_URL}/vaccines`)
-      .then((response) => response.json())
-      .then((data) => {
-        // Clear previous table content
-        var tableContainer = document.getElementById("vaccinationsTableContainer");
-        tableContainer.innerHTML = "";
-  
-        // Create table header
-        var table = document.createElement("table");
-        var headerRow = table.insertRow();
-        var headers = [
-          "VaccineID",
-          "PersonID",
-          "FacilityID",
-          "VaccinationType",
-          "VaccinationDate",
-          "DoseNumber",
-          "",
-          "",
-        ]; // Example headers, replace with your actual data keys
-        headers.forEach((header) => {
-          var th = document.createElement("th");
-          th.textContent = header;
-          headerRow.appendChild(th);
-        });
-  
-        // Populate table with data
-        data.vaccinations.forEach((vaccination) => {
-          var row = table.insertRow();
-          Object.entries(vaccination).forEach(([key, value]) => {
-            var cell = row.insertCell();
-            if (key == "VaccinationDate") {
-              cell.textContent = formatDate(value);
-            } else {
-              cell.textContent = value;
-            }
-          });
-  
-          // Add delete button to each row
-          var deleteButtonCell = row.insertCell();
-          var deleteButton = document.createElement("button");
-          deleteButton.textContent = "Delete";
-          deleteButton.addEventListener("click", function () {
-            if (confirm("Are you sure you want to delete this Vaccination?")) {
-              deleteVaccination(vaccination.VaccineID);
-              table.deleteRow(row.rowIndex);
-            }
-          });
-          deleteButtonCell.appendChild(deleteButton);
-  
-          // Add update button to each row
-          var updateButtonCell = row.insertCell();
-          var updateButton = document.createElement("button");
-          updateButton.textContent = "Update";
-          updateButton.addEventListener("click", function () {
-            fillUpdateFormWithVaccinationData(vaccination);
-          });
-          updateButtonCell.appendChild(updateButton);
-        });
-  
-        // Append table to table container
-        tableContainer.appendChild(table);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
+  fetch(`${BASE_URL}/vaccines`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Clear previous table content
+      var tableContainer = document.getElementById(
+        "vaccinationsTableContainer"
+      );
+      tableContainer.innerHTML = "";
+
+      // Create table header
+      var table = document.createElement("table");
+      var headerRow = table.insertRow();
+      var headers = [
+        "VaccineID",
+        "PersonID",
+        "FacilityID",
+        "VaccinationType",
+        "VaccinationDate",
+        "DoseNumber",
+        "",
+        "",
+      ]; // Example headers, replace with your actual data keys
+      headers.forEach((header) => {
+        var th = document.createElement("th");
+        th.textContent = header;
+        headerRow.appendChild(th);
       });
-  }
+
+      // Populate table with data
+      data.vaccinations.forEach((vaccination) => {
+        var row = table.insertRow();
+        Object.entries(vaccination).forEach(([key, value]) => {
+          var cell = row.insertCell();
+          if (key == "VaccinationDate") {
+            cell.textContent = formatDate(value);
+          } else {
+            cell.textContent = value;
+          }
+        });
+
+        // Add delete button to each row
+        var deleteButtonCell = row.insertCell();
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function () {
+          if (confirm("Are you sure you want to delete this Vaccination?")) {
+            deleteVaccination(vaccination.VaccineID);
+            table.deleteRow(row.rowIndex);
+          }
+        });
+        deleteButtonCell.appendChild(deleteButton);
+
+        // Add update button to each row
+        var updateButtonCell = row.insertCell();
+        var updateButton = document.createElement("button");
+        updateButton.textContent = "Update";
+        updateButton.addEventListener("click", function () {
+          fillUpdateFormWithVaccinationData(vaccination);
+        });
+        updateButtonCell.appendChild(updateButton);
+      });
+
+      // Append table to table container
+      tableContainer.appendChild(table);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
 
 function createVaccination(event) {
   const form = document.getElementById("createVaccinationForm");
@@ -158,20 +160,23 @@ function updateVaccination(event) {
     });
 }
 
-  function fillUpdateFormWithVaccinationData(vaccination) {
-    document.getElementById("vaccineIdToUpdate").value = vaccination.VaccineID;
-    document.getElementById("personIDToUpdate").value = vaccination.PersonID;
-    document.getElementById("facilityIDToUpdate").value = vaccination.FacilityID;
-    document.getElementById("vaccinationTypeToUpdate").value = vaccination.VaccinationType;
-    document.getElementById("vaccinationDateToUpdate").value = formatDate(vaccination.VaccinationDate);
-    document.getElementById("doseNumberToUpdate").value = vaccination.DoseNumber;
-  }
+function fillUpdateFormWithVaccinationData(vaccination) {
+  document.getElementById("vaccineIdToUpdate").value = vaccination.VaccineID;
+  document.getElementById("personIDToUpdate").value = vaccination.PersonID;
+  document.getElementById("vaccinationFacilityIDToUpdate").value =
+    vaccination.FacilityID;
+  document.getElementById("vaccinationTypeToUpdate").value =
+    vaccination.VaccinationType;
+  document.getElementById("vaccinationDateToUpdate").value = formatDate(
+    vaccination.VaccinationDate
+  );
+  document.getElementById("doseNumberToUpdate").value = vaccination.DoseNumber;
+}
 
-  function formatDate(dateToFormat) {
-    const date = new Date(dateToFormat);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
-  }
-  
+function formatDate(dateToFormat) {
+  const date = new Date(dateToFormat);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
